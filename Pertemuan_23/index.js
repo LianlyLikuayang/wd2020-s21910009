014ce029f87a56623d727ff5c3c8a22528c32ef6
LianlyLikuayang <s21910009@student.unklab.ac.id>
2020-11-10 00:45:57 +0800

    Exercise 7 Variabel Operator JavaScript

---
+++ Pertemuan_23/index.js
@@ -0,0 +1,74 @@
+//Variabel
+
+//Let & Const
+// let _umur = 25;
+// _umur = 26;
+// let niai = 95.5;
+// let $nama = "John Doe";
+// console.log(_umur);
+// console.log($nama);
+
+// let alamat;
+// console.log(alamat);
+
+// const tahunLahir = 2010;
+// console.log(tahunLahir);
+
+//Oerator
+//Arithmatic Operator (+, -, *, /)
+// let angka1 = 10;
+// let angka2 = 20;
+// console.log(angka1+angka2);
+
+// let umur = 30;
+// let namaDepan = "John";
+// let namaBelakang = "Doe";
+// console.log("Nama saya " + namaDepan + " " + namaBelakang + ". Umur saya adalah " + umur + " tahun");
+
+//Operator Perbandingan
+// let angka1 = 10;
+// let angka2 = 9;
+// console.log(angka1 <= angka2);
+
+//Ternary Operator
+// let angka = 5;
+// let hasil = angk <= 0 ? "Negatif" : "Positif";
+// console.log(hasil);
+
+//Operator Logika
+// let kondisi1 = 1 < 2;
+// let kondisi2 = true;
+// console.log(kondisi1 || kondisi2);
+// console.log(!kondisi1);
+
+// let angka = prompt("Masukkan angka : ");
+// console.log("Angka yang dimasukkan adalah " + angka);
+
+//Exercise #7
+/*
+    Buatlah program JavaScript untuk menghitung BMI ( Body Mass Index).
+    Rumus = berat (kg) / (tinggi*tinggi)
+
+    Gunakan prompt untuk meminta input dari user
+*/
+
+// let berat = prompt("Masukkan Berat Badan : ");
+// console.log("Berat yang dimasukkan adalah " + berat);
+// let tinggi = prompt("Masukkan Tinggi Badan : ");
+// console.log("Tinggi yang dimasukkan adalah " + tinggi);
+
+// console.log(berat/ tinggi * tinggi);
+
+let BeratBadan = prompt("Masukan berat : ");
+console.log("Berat anda adalah : " + BeratBadan);
+
+let TinggiBadan = prompt("Masukan tinggi : ");
+console.log("Tinggi anda adalah : " + TinggiBadan);
+
+let Bil1 = BeratBadan;
+let BIl2 = TinggiBadan;
+let hasil;
+
+console.log(hasil = BIl2 * BIl2);
+console.log(Bil1 / hasil);
+
